59a3a70952d6cf5fe5ffddf239737a722f6fc049
Mostafa Eid <lost.mostafa92@gmail.com>
2022-01-29 03:36:19 +0200

    initial release

--- src/Commands/Cp.php
+++ src/Commands/Cp.php
@@ -4,10 +4,11 @@ namespace Liteas98\Cp\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schema;
+use Liteas98\Cp\CpInstallServiceProvider;
 
 class Cp extends Command
 {
-    protected $signature = 'cp:install {dashboard}';
+    protected $signature = 'cp:install';
     protected $description = 'Install & Publish Dashboard';
 
     public function __construct()
@@ -16,12 +17,7 @@ class Cp extends Command
     }
 
     public function handle() {
-        $dashboard = $this->argument('dashboard');
-//        if ($dashboard == 'vuexy'){
-//            $this->publishes([__DIR__ . '/../dashboard/vuexy/app' => base_path('app')]);
-//            $this->publishes([__DIR__ . '/../dashboard/vuexy/public' => base_path('public')]);
-//            $this->publishes([__DIR__ . '/../dashboard/vuexy/resources' => base_path('resources')]);
-//            $this->publishes([__DIR__ . '/../dashboard/vuexy/routes' => base_path('routes')]);
-//        }
+        $dashboard = config('dashboard.dashboard');
+        return new CpInstallServiceProvider($dashboard);
     }
 }
